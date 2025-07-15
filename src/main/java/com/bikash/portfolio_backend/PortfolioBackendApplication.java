1b4bcf6b6a3d09fb7d9d486065494cb4d97b0562
Bikash Chauhan <63906998+bikash-789@users.noreply.github.com>
2025-07-16 01:18:39 +0530

    add timezone

--- src/main/java/com/bikash/portfolio_backend/PortfolioBackendApplication.java
+++ src/main/java/com/bikash/portfolio_backend/PortfolioBackendApplication.java
@@ -5,13 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableMongoAuditing
 @EnableScheduling
 public class PortfolioBackendApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		System.out.println("Application timezone set to: " + TimeZone.getDefault().getDisplayName());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
-
 }
